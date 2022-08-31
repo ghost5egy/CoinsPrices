bb17d4f3b773299da6ae495234ea3830fcc3282a
Ahmed Saeed <ahmedsaeedramadan@yahoo.com>
2022-08-31 04:00:46 +0200

    Update precache-manifest.5a7ba092e171ae97b6ad2eb8d21edc83.js

--- precache-manifest.5a7ba092e171ae97b6ad2eb8d21edc83.js
+++ precache-manifest.5a7ba092e171ae97b6ad2eb8d21edc83.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
     "revision": "86fb538a9a99343e01f3",
-    "url": "CryptoPrices/static/css/main.eda477db.chunk.css"
+    "url": "static/css/main.eda477db.chunk.css"
   },
   {
     "revision": "86fb538a9a99343e01f3",
-    "url": "CryptoPrices/static/js/main.86fb538a.chunk.js"
+    "url": "static/js/main.86fb538a.chunk.js"
   },
   {
     "revision": "10e59a8baf6e9a2d1eea",
-    "url": "CryptoPrices/static/js/1.10e59a8b.chunk.js"
+    "url": "static/js/1.10e59a8b.chunk.js"
   },
   {
     "revision": "79d0b3ccbd257978e64d",
-    "url": "CryptoPrices/static/js/2.79d0b3cc.chunk.js"
+    "url": "static/js/2.79d0b3cc.chunk.js"
   },
   {
     "revision": "62ace6afc88b5865449a",
-    "url": "CryptoPrices/static/js/runtime~main.62ace6af.js"
+    "url": "static/js/runtime~main.62ace6af.js"
   },
   {
     "revision": "db707272f96829407ed512110a52e234",
-    "url": "CryptoPrices/index.html"
+    "url": "index.html"
   }
 ];
