a48fd4dfc42eed146b3fac854be1520b62b8b074
Ahmed Saeed <ahmedsaeedramadan@yahoo.com>
2022-08-31 04:02:21 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "CryptoPrices/precache-manifest.5a7ba092e171ae97b6ad2eb8d21edc83.js"
+  "precache-manifest.5a7ba092e171ae97b6ad2eb8d21edc83.js"
 );
 
 workbox.clientsClaim();
